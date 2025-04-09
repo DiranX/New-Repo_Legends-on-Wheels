edcd95d075811602db7db9c768d8318bc190bb4d
DiranX <fikar.firdi.fkr@gmail.com>
2025-04-10 00:42:30 +0700

    Arena 2 tambahan

--- Assets/Firdi/Scripts/WaterFX.cs
+++ Assets/Firdi/Scripts/WaterFX.cs
@@ -1,25 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class WaterFX : MonoBehaviour
 {
-    [SerializeField] GameObject waterfx;
-
+    [SerializeField] GameObject waterFog;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("MainCamera"))
+        if (other.CompareTag("Water"))
         {
             //waterfx.gameObject.SetActive(true);
-            RenderSettings.fog = true;
+            //RenderSettings.fog = true;
+            waterFog.SetActive(true);
         }
     }
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("MainCamera"))
+        if (other.CompareTag("Water"))
         {
             //waterfx.gameObject.SetActive(false);
-            RenderSettings.fog = false;
+            //RenderSettings.fog = false;
+            waterFog.SetActive(false);
         }
     }
 }
